a908e4c640d068069df3262544a9ec935a91d95b
Prabhudeba <prabhudeba1111@gmail.com>
2026-03-01 02:33:59 +0530

    added the final scene and updated player

--- Scripts/Multiplayer/network_ui.gd
+++ Scripts/Multiplayer/network_ui.gd
@@ -1,9 +1,19 @@
 extends Control
 
+@onready var host_button: Button = $VBoxContainer/HostButton
+@onready var join_button: Button = $VBoxContainer/JoinButton
 
 func _on_host_button_pressed() -> void:
 	NetworkHandler.start_server()
+	hide_ui()
 
 
 func _on_join_button_pressed() -> void:
 	NetworkHandler.start_client()
+	hide_ui()
+
+
+func hide_ui() -> void:
+	host_button.disabled = true
+	join_button.disabled = true
+	visible = false
--- Scripts/Players/player.gd
+++ Scripts/Players/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
-@export var speed := 6.0
-@onready var camera: Node3D = $Camera
+@export var speed := 10.0
+@export var jump_velocity = 10.0
 
+@onready var camera: Node3D = $Camera3D
+
+var gravity = 20.0
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
@@ -11,9 +14,27 @@ func _enter_tree():
 func _ready():
 	if not is_multiplayer_authority():
 		set_physics_process(false)
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
+
+func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
+	
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * .005)
+		camera.rotate_x(-event.relative.y * .005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+	
 	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input.x, 0, input.y)).normalized()
 	velocity.x = direction.x * speed
